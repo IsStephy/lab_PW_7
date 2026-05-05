7b82f57a17a75f66aa1b90213124159340a5853a
IsStephy <145896071+IsStephy@users.noreply.github.com>
2026-05-06 00:50:23 +0300

    fix: correct GitHub Pages base path to lab_PW_7

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/lab_PW_6/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/lab_PW_7/' : '/',
   server: {
     host: true,
     port: 5173,
